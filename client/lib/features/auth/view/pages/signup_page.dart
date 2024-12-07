126e91e161577cae6d7fc68c4d2d1de730240125
Anass <chawkianas75@gmail.com>
2024-12-08 00:32:34 +0100

    configured messages after signup

--- client/lib/features/auth/view/pages/signup_page.dart
+++ client/lib/features/auth/view/pages/signup_page.dart
@@ -31,6 +31,41 @@ class _SignupPageState extends ConsumerState<SignupPage> {
   @override
   Widget build(BuildContext context) {
     final isLoading = ref.watch(authViewmodelProvider)?.isLoading == true;
+    // the part that changes pages from signup to login
+    ref.listen(
+      authViewmodelProvider,
+      (_, next) {
+        next?.when(
+          data: (data) {
+            ScaffoldMessenger.of(context)
+              ..hideCurrentSnackBar()
+              ..showSnackBar(
+                const SnackBar(
+                  content: Text('Account created successfully! Please login.'),
+                ),
+              );
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const LoginPage(),
+              ),
+            );
+          },
+          error: (error, st) {
+            ScaffoldMessenger.of(context)
+              ..hideCurrentSnackBar()
+              ..showSnackBar(
+                SnackBar(
+                  content: Text(
+                    error.toString(),
+                  ),
+                ),
+              );
+          },
+          loading: () {},
+        );
+      },
+    );
 
     return Scaffold(
       appBar: AppBar(),
